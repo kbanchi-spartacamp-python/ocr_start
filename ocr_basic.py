807d8e3cf762ebb697b70fdb1ec00eade1246dfc
kbanchi1111 <kbanchi.1111@gmail.com>
2022-11-12 11:43:10 +0900

    add ocr

---
+++ ocr_basic.py
@@ -0,0 +1,15 @@
+from PIL import Image
+import pyocr
+
+tools = pyocr.get_available_tools()
+tool = tools[0]
+
+# print(tool)
+# print(tool.get_name())
+
+img = Image.open("sparta_camp_en.png")
+# img.show()
+
+txt = tool.image_to_string(img, lang="eng+jpn", builder=pyocr.builders.TextBuilder())
+
+print(txt)
